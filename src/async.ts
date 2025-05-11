0f1ae4d7920e0be3ae762939705c50173229ba46
Munir Hussin <profoundseven@gmail.com>
2025-05-11 21:53:49 +0800

    updated async functions

--- src/async.ts
+++ src/async.ts
@@ -1,7 +1,9 @@
-export function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+export function sleep(ms: number): Promise<void>;
+export function sleep<T>(ms: number, value: T): Promise<T>;
+export function sleep<T>(ms: number, value?: T): Promise<T> {
+    return new Promise(pass => setTimeout(() => pass(value as T), ms));
 }
 
-export function timeout<T = void>(ms: number) {
-    return new Promise<T>((_, reject) => setTimeout(() => reject(new Error("timeout")), ms));
+export function timeout<T = void>(ms: number, error?: any): Promise<T> {
+    return new Promise((_, fail) => setTimeout(() => fail(error ?? new Error("timeout")), ms));
 }
